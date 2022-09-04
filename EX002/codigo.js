6aa7e97d207cf49c6f7e7f1512a5c242f8016978
Mateus Marques <MateusMarques53@outlook.com>
2022-09-04 17:57:26 -0300

    Variaveis e expressoes

---
+++ EX002/codigo.js
@@ -0,0 +1,62 @@
+
+ /*/ 
+ Exemplos de escrita
+ document.write = escrita na tela
+ /*/
+
+var y;
+
+y=37;
+document.write(y);
+document.write("<br>");
+
+y="Mateus Marques";
+document.write(y);
+document.write("<br>");
+
+y=true;
+document.write(y);
+document.write("<br>");
+
+y=null;
+document.write(y);
+document.write("<br>");
+
+/*/ 
+Exeplos de Variaveis 
+
+Expressão matematica simples
+
+definir a var, a expressao e atrelado a var, depois mostrar na tela, depois da var(expressao)
+/*/
+
+var expressao;
+
+expressao = 30 + 20 -10*5;
+
+document.write(" 30 + 20 - 10* 5 = ");
+document.write(expressao);
+
+/*/
+Operadores
++ = soma
+- = subtração
+* = multiplicação
+/ = divisão
+% = resto da visao
+++ = incremento
+-- = decremento
+
+Operadores de comparação
+== = valor igual
+=== = valor e tipo são iguais
+!= = Diferente
+!== = Valor e tipo diferente
+> = maior que
+< = menor que
+>= = maior ou igual
+<= = menor ou igual
+&& = "E" logico (and)
+|| = "OU" logido (or)
+! = negação (not)
+/*/
